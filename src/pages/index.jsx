9dd89755c7a914241f721ff21b5fe87013f78f4a
Steffen <ring@kitconcept.com>
2020-04-22 17:12:16 +0200

    first steps of developement

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import { Header } from '../layouts';
-import editorPreviewImg from '../static/editor-preview.png';
-import iconsPreviewImg from '../static/icons-preview.png';
+import React from "react"
+import Link from "gatsby-link"
+import { Header } from "../layouts"
+import editorPreviewImg from "../static/editor-preview.png"
+import iconsPreviewImg from "../static/icons-preview.png"
 
 const IndexPage = () => (
   <div className="view">
@@ -14,11 +14,11 @@ const IndexPage = () => (
           <h2>The ulimate CMS for editing the web</h2>
         </div>
         <div className="image-wrapper">
-        <img
-          className="pastanaga-editor-preview"
-          src={editorPreviewImg}
-          alt="pastanaga-editor-preview"
-        />
+          <img
+            className="pastanaga-editor-preview"
+            src={editorPreviewImg}
+            alt="pastanaga-editor-preview"
+          />
         </div>
       </div>
     </div>
@@ -29,18 +29,49 @@ const IndexPage = () => (
         <p>Lorem Ipsum</p>
       </div>
     </div>
+    <div className="demo">
+      <h1>Volto Demo</h1>
+      <h2>
+        Jump into Volto and try out our Demo!<br></br>Try it{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://volto.kitconcept.com"
+        >
+          here
+        </a>
+      </h2>
+      <div>
+        <p>Lorem Ipsum</p>
+      </div>
+    </div>
+    <div className="roadmap">
+      <h1>Roadmap</h1>
+      <h2>Timeline...</h2>
+      <div className="timeline" style={{ width: `900px` }}></div>
+    </div>
+    <div className="open-source">
+      <h1>Open Source</h1>
+      <h2>
+        Volto is an Open Source project, everybody can participate and use it.
+        There are no fees or anything!
+      </h2>
+      <div>
+        <p>Lorem Ipsum</p>
+      </div>
+    </div>
     <div className="footer-wrapper">
-    <footer>
-      <p>
-        The <a href="https://plone.org">Plone</a> community
-      </p>
-      <p>The Guillotina community</p>
-      <p>
-        Hosted by <a href="https://kitconcept.com">kitconcept</a>
-      </p>
-  </footer> 
+      <footer>
+        <p>
+          The <a href="https://plone.org">Plone</a> community
+        </p>
+        <p>The Volto community</p>
+        <p>
+          Hosted by <a href="https://kitconcept.com">kitconcept</a>
+        </p>
+      </footer>
+    </div>
   </div>
-  </div>
-);
+)
 
-export default IndexPage;
+export default IndexPage
